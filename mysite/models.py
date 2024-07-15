05bb13f8550f8120b1008a70a802213497329463
Xiaowang <danny20041212@gmail.com>
2024-07-15 16:05:44 +0800

    blog complete

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200) #最大可儲存字元200
+    slug = models.CharField(max_length=200) #最大可儲存字元200
+    body = models.TextField() #會超過255字元都這樣表示
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title
